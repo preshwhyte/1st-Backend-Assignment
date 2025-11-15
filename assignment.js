85efce7ec8f73f50e1cc9d0e5889f6c268589ddd
unknown <preshwhyte1@gmail.com>
2025-11-15 03:41:12 -0800

    first commit

---
+++ assignment.js
@@ -0,0 +1,49 @@
+// 1. Declare a variable using let and assign your name to it.
+let name='Precious Nwogu';
+
+console.log(name)
+// 2. Write JavaScript code to add two numbers and log the result.
+var addnumber=1+2
+console.log(addnumber)
+// 3. Create an object called student with properties: name, age, and department.
+const student = {
+  name: "precious nwogu",
+  age: 32,
+  department: "Medical Sciences"
+};
+
+// 4. Write a JavaScript function called greet() that prints "Hello World".
+function greet(){
+    console.log("Hello World")
+}
+greet()
+
+// 5. Write a program that checks if a number is even or odd.
+let program=4%2
+
+if (program==0){
+    console.log("even number")
+}
+else{
+    console.log("odd number")
+}
+
+// 6. Create an array of 5 colors and print the first color
+
+const colors=["yellow", "green", "black", "white", "brown"]
+console.log(colors[0])
+
+// 7.Write a function that returns the square of a number passed into it.
+
+function square(a){
+    return a*a
+}
+
+console.log(square(2))
+
+// 8. Write a small code snippet that converts a string to a number:
+let value = "42";
+let numberValue = Number(value);
+console.log(value)
+
+
